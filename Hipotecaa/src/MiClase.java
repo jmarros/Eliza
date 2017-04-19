1a8a6eb4ce35db59a9acc45556d392da061b173b
Jesús <Jess@LAPTOP-7RF7MK5H>
2017-04-19 09:20:51 +0200

    Version inicial

---
+++ Hipotecaa/src/MiClase.java
@@ -0,0 +1,41 @@
+
+	
+
+	public MiClase(int valorInicial) {
+		this.acumulado = valorInicial;
+		private int acumulado;
+	}
+
+	public int getAcumulado() {
+		return acumulado;
+	}
+
+	public void setAcumulado(int valor) {
+		this.acumulado = valor;
+	}
+	
+	public void incrementa() {
+		this.acumulado++;
+	}
+	
+	public void decrementa() {
+		this.acumulado--;
+	}
+
+	public void suma(int valor) {
+		this.acumulado += valor;
+	}
+	
+	public void resta(int valor) {
+		suma(-valor);
+	}
+	
+	public int sumaDevuelve(int valor) {
+		suma(valor);
+		return getAcumulado();
+	}
+	
+	public int restaDevuelve(int valor) {
+		return sumaDevuelve(-valor);
+	}	
+}
---
+++ Hipotecaa/src/StartApp.java
@@ -0,0 +1,16 @@
+
+public class Startapp{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MiClase o1 = new MiClase();
+		o1.incrementa();
+		o1.suma(10);
+		o1.resta(10);
+		System.out.println(o1.sumaDevuelve(1));
+	}
+
+}
